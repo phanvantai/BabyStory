e262d5db7112be050b71e9637ef59a87caf44183
Tai Phan Van <taipv.swe@gmail.com>
2025-05-28 01:16:20 +0700

    Initial Commit

---
+++ BabyStory/BabyStoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabyStoryApp.swift
+//  BabyStory
+//
+//  Created by Tai Phan Van on 28/5/25.
+//
+
+import SwiftUI
+
+@main
+struct BabyStoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabyStory/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BabyStory
+//
+//  Created by Tai Phan Van on 28/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
